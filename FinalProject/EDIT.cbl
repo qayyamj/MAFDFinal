e0435e4fa8abdd72b36bb7bb2bd85bcf79860e66
Menu <syncrow123@gmail.com>
2021-03-31 11:09:25 -0400

    Worked more on the main file. waiting for *someone* to format how it would look like

--- FinalProject/EDIT.cbl
+++ FinalProject/EDIT.cbl
@@ -30,11 +30,11 @@
            data record is data-record.
 
        01 data-record.
-         05 transaction-code               pic x.
+         05 transaction-code               pic X.
            88 ws-valid-code 
                value "S", "R", "L".
          05 transaction-amount             pic 9(5)V99.
-         05 payment-type                   pic xx.
+         05 payment-type                   pic XX.
            88 ws-valid-pay-type
                value "CA", "CR", "DB".
          05 store-number                   pic XX.
@@ -49,7 +49,7 @@
            10 invoice2 pic x.
              88 ws-valid-invoice2                         
                values "A", "B", "C", "D", "E".                            
-           10 invoice-dash                 pic x.
+           10 invoice-dash                 pic X.
            10 invoice-numbers              pic 9(6).
              88 valid-nums
                value 100000        thru 900000.
@@ -80,15 +80,53 @@
        01 ws-eof-flag                      pic x
                value "N".
 
+       01 ws-detail-line.
+         05 ws-trans-code                  pic X.
+         05 ws-trans-amount                pic 9(5)V99.
+         05 ws-pay-type                    pic XX.
+         05 ws-store-num                   pic XX.
+         05 ws-invoice-num                 pic x(9).
+         05 ws-sku-code                    pic x(15).
 
+       01 ws-detail-invalid-line.
+         05 ws-invalid-trans-code          pic X.
+         05 ws-invalid-trans-amount        pic 9(5)V99.
+         05 ws-invalid-pay-type            pic XX.
+         05 ws-invalid-store-num           pic XX.
+         05 ws-invalid-invoice-num         pic x(9).
+         05 ws-invalid-sku-code            pic x(15).
 
+       01 ws-error-report-total.
+         05 filler                         pic x(15)
+               value "Total Records: ".
+         05 filler                         pic x(5)
+               value spaces.
+         05 ws-error-report-total          pic Z99
+               value 0.
 
+       01 ws-error-report-valid.
+         05 filler                         pic x(21)
+               value "Total Valid Records: ".
+         05 filler                         pic x(5)
+               value spaces.
+         05 ws-error-valid-total          pic Z99
+               value 0.
+
+       01 ws-error-report-invalid.
+         05 filler                         pic x(23)
+               value "Total Invalid Records: ".
+         05 filler                         pic x(5)
+               value spaces.
+         05 ws-error-invalid-total          pic Z99
+               value 0.
 
        procedure division.
        000-main.
 
            open input input-file.
-           
+           open output valid-file, invalid-file, errors-file.
+
+           close input-file, valid-file, invalid-file, errors-file.
            goback.
            
        end program EDIT.
